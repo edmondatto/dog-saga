212513bcc969084f12773da1d787f65baa90151c
Edmond Atto <edmond091@gmail.com>
2018-09-06 14:54:07 +0300

    Add reducer

--- src/redux.js
+++ src/redux.js
@@ -2,3 +2,40 @@
 const API_CALL_REQUEST = 'API_CALL_REQUEST';
 const API_CALL_FAILURE = 'API_CALL_FAILURE';
 const API_CALL_SUCCESS = 'API_CALL_SUCCESS';
+
+// reducer with initial state
+const initialState = {
+  fetching: false,
+  dog: null,
+  error: null
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case action.type === API_CALL_REQUEST:
+      return {
+        ...state,
+        fetching: true,
+        error: null
+      };
+
+    case action.type === API_CALL_FAILURE:
+      return {
+        ...state,
+        fetching: null,
+        error: action.error,
+        dog: null
+      };
+
+    case action.type === API_CALL_SUCCESS:
+      return {
+        ...state,
+        fetching: null,
+        error: null,
+        dog: action.dog
+      };
+
+    default:
+      return state;
+  }
+}
