842e8d0b1dd93a6740cb7f9b997e5520382a922b
Edmond Atto <edmond091@gmail.com>
2018-09-06 15:27:28 +0300

    Connect app to redux and redux-saga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, applyMIddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
+
+// Create the Saga Middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// DevTools Middleware
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION();
+
+// Create the Redux Store
+let store = createStore(
+  reducer,
+  compose(applyMIddleware(sagaMiddleware), reduxDevTools)
+);
+
+// Run the saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
